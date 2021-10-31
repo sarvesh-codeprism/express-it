c2c85ab6413af6f2d7e4b564dfaf1e85ddcc64f0
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-10-31 22:17:00 +0530

    Add views and routes

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const articleRouter = require("./routes/articles");
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+app.use("/articles", articleRouter);
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Test title",
+      createdAt: Date.now(),
+      description: "Test description",
+    },
+    {
+      title: "Test2 title",
+      createdAt: Date.now(),
+      description: "Test2 description",
+    },
+  ];
+  res.render("index", { articles: articles });
+});
 
-app.listen(3000)
+app.listen(3000);
